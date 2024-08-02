45e42efd67bba8cf5f250028739ad747152d11a2
AryanDTU27 <aryanrajputofficial@outlook.com>
2024-08-03 01:46:17 +0530

    learnt about threejs editor

--- start/lecture3_4/app.js
+++ start/lecture3_4/app.js
@@ -11,11 +11,23 @@ class App{
         
 		this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0xaaaaaa );
+
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+		this.scene.add(ambient);
+
+		const light = new THREE.DirectionalLight();
+		light.position.set( 0.2, 1, 1);
+		this.scene.add(light);
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		container.appendChild( this.renderer.domElement );
+
+		const geometry = new THREE.BoxBufferGeometry();
+		const material = new THREE.MeshStandardMaterial(0xff0000);
+		this.mesh = new THREE.Mesh(geometry, material);
+		this.scene.add(this.mesh);
 		       
         this.renderer.setAnimationLoop(this.render.bind(this));
     
@@ -23,10 +35,13 @@ class App{
 	}	
     
     resize(){
-
+		this.camera.aspect = window.innerWidth/window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
     
 	render( ) {   
+		this.mesh.rotateX(0.01);
 		this.renderer.render( this.scene, this.camera );
     }
 }
